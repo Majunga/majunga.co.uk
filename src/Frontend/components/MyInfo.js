ff5d91137a04a2e508c4ce6413a479f52df3afe5
Ethan McWilliams <ethan.mcwilliams@pharmacy2u.co.uk>
2018-09-08 21:43:06 +0100

    Added more information about myself

--- src/Frontend/components/MyInfo.js
+++ src/Frontend/components/MyInfo.js
@@ -1,11 +1,12 @@
 import {Jumbotron} from 'react-bootstrap'
-
+import Link from 'next/link'
 const MyInfo = () =>
 
 <div>
     <Jumbotron >
         <h1>Hello, <p>My name is Ethan and welcome to my website.</p></h1>
-        <p>This website was made to display things about myself and what I've been creating.</p>
+        <p>This website was made to display things about myself and what I've been creating. 
+            If you would like to find out more about me, please head to my <Link href="https://www.linkedin.com/in/ethan-mcwilliams-92a02a3b/"><a>LinkedIn</a></Link></p>
     </Jumbotron>
 </div>
 
