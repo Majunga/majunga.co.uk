310f7e1f5ca5b846d8cda6edb71a35f6539946ad
Ethan McWilliams <ethan.mcwilliams@pharmacy2u.co.uk>
2018-09-09 10:42:16 +0100

    Fix to the order of the repos displayed

--- src/Frontend/components/Portfolio.js
+++ src/Frontend/components/Portfolio.js
@@ -7,25 +7,27 @@ export default class Portfolio extends React.Component {
             var json = await (await fetch('https://api.github.com/users/majunga/repos')).json()
             this.setState({repos: json})
                
-         }
+    }
     render() {
         return (
             <div className="portfolio">
                 <h2><Link href="https://github.com/Majunga"><a>My Github</a></Link></h2>
                 
-                {(this.state && this.state.repos && this.state.repos.length > 0) ? 
-                <div>
-                    
-                     { 
-                         this.state.repos.map(repo =>
-                            <div key="{repo.id}">
-                                <h3><Link href={repo.html_url}><a>{repo.name}</a></Link></h3>
-                                <p>{repo.description}</p>
-                            </div>
-                        )
-                    }
-                </div>
-                : <p>No Repos to display</p>}
+                {
+                    (this.state && this.state.repos && this.state.repos.length > 0) ? 
+                    <div>
+                        { 
+                            this.state.repos.reverse(repo => repo.updated_at).map(repo =>
+                                <div key="{repo.id}">
+                                    <h3><Link href={repo.html_url}><a>{repo.name}</a></Link></h3>
+                                    <p>{repo.description}</p>
+                                </div>
+                            )
+                        }
+                    </div>
+                    : <p>No Repos to display</p>
+                }
+
                 <style jsx>{`
                         a {
                             color : #333;
