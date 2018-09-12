f89f326026e9fc110ea5b0080fb8bc2e68e34cb7
Ethan McWilliams <ethan.mcwilliams@pharmacy2u.co.uk>
2018-09-12 22:28:36 +0100

    Added more logging

--- src/Bot/app.js
+++ src/Bot/app.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log(process.env.msAppId)
+    console.log('%s listening to %s', server.name, server.url); 
 });
 
 // Create chat connector for communicating with the Bot Framework Service
@@ -19,7 +20,7 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    console.log("Message received")
-    console.log(session.message.text)
+    console.log(session.message.localTimestamp + " Message received")
     session.send("You said: %s", session.message.text);
+    console.log("Message sent")
 }).set('storage', new builder.MemoryBotStorage());
