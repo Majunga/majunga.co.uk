5c3d3ad67bd51f148edd5dc4a8e5a1c1894381a7
Ethan McWilliams <ethan.mcwilliams@pharmacy2u.co.uk>
2018-09-13 13:04:53 +0100

    Added bot to the front of all commands to make it So that it doesn't repond to everything.

--- src/Bot/app.js
+++ src/Bot/app.js
@@ -28,10 +28,10 @@ server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector, function (session) {
     console.log(GetDate() + " Message received")
     var message = session.message.text.toLocaleLowerCase()
-    if (message.startsWith("download")) {
+    if (message.startsWith("bot download")) {
         DownloadFromYoutube(session, message)
     }
-    else {
+    else if(message.startsWith("bot")) {
         HelpMessage(session)
     }
     console.log(GetDate() + " Message sent")
@@ -39,14 +39,14 @@ var bot = new builder.UniversalBot(connector, function (session) {
 
 HelpMessage = (session) => {
     session.send("Bot Help. To use this bot please type one of the following commands:");
-    session.send("download [Youtube URL]");
+    session.send("bot download [Youtube URL]");
 }
 
 DownloadFromYoutube = (session, message) => {
     var splitMessage = message.split(" ")
 
-    if (splitMessage.length == 2) {
-        var url = splitMessage[1].replace("<", "").replace(">", "")
+    if (splitMessage.length == 3) {
+        var url = splitMessage[2].replace("<", "").replace(">", "")
         var id = url.split("?v=")
         console.log("Downloading " + id[id.length - 1] + ".mp4 from " + url)
         session.send("Downloading video")
