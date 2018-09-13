921d91597a7de2dd3cdb81ff40935f42757644cb
Ethan McWilliams <ethan.mcwilliams@pharmacy2u.co.uk>
2018-09-13 12:04:29 +0100

    Added downloader for Youtube

--- src/Bot/app.js
+++ src/Bot/app.js
@@ -1,14 +1,19 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 require('dotenv').config();
+const { exec } = require('child_process');
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log(process.env.MicrosoftAppId)
     console.log(process.env.MicrosoftAppPassword)
-    console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
+
+server.get("/static/*", restify.plugins.serveStatic({
+    directory: __dirname
+}));
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
@@ -22,18 +27,44 @@ server.post('/api/messages', connector.listen());
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
     console.log(GetDate() + " Message received")
-    session.send("You said: %s", session.message.text);
+    var message = session.message.text
+    if (message.toLocaleLowerCase().startsWith("download")) {
+        DownloadFromYoutube(session, message)
+    }
+    else {
+        session.send("You said: %s", session.message.text);
+    }
     console.log(GetDate() + " Message sent")
 }).set('storage', new builder.MemoryBotStorage());
 
+DownloadFromYoutube = (session, message) => {
+    var splitMessage = message.split(" ")
 
+    if (splitMessage.length == 2) {
+        var url = splitMessage[1]
+        var id = url.split("?v=")
+        console.log("Downloading " + id + ".mp4 from " + url)
+
+        exec('youtube-dl -o "./static/%(id)s.%(ext)s" ' + url , (err, stdout, stderr) => {
+            if (err) {
+                console.log("Failed to save video: " + err)
+                session.send("Failed to save video: " + err)
+                return
+            }
+            else {
+                console.log("https://majunga.co.uk:3978/static/"+ id[id.length -1] + ".mp4")
+                session.send("https://majunga.co.uk:3978/static/"+ id[id.length -1] + ".mp4")
+            }
+        })
+    }
+}
 
 GetDate = () => {
     var currentdate = new Date()
     return currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds()
+        + (currentdate.getMonth() + 1) + "/"
+        + currentdate.getFullYear() + " @ "
+        + currentdate.getHours() + ":"
+        + currentdate.getMinutes() + ":"
+        + currentdate.getSeconds()
 }
