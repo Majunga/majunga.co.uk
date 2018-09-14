c9b2e07958d2bad3083b6a148084ec474e328fef
Ethan McWilliams <ethan.mcwilliams@pharmacy2u.co.uk>
2018-09-14 22:23:31 +0100

    Added File clean up methods and the start of FFMpeg service

---
+++ src/BotDot/BusinessLogic/Services/FfMpeg.cs
@@ -0,0 +1,24 @@
+﻿namespace BotDot.BusinessLogic.Services
+{
+    using BotDot.BusinessLogic.Services.Interfaces;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class FFMpeg : IVideoConverter
+    {
+        private string outputPath;
+
+        public FFMpeg(string outputPath)
+        {
+            this.outputPath = outputPath;
+        }
+
+        public FileInfo ConvertToMp4(FileInfo file, Tuple<string, string> times)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/BotDot/Helpers/FileHelper.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="FileHelper.cs" company="Majunga.co.uk">
+// Copyright (c) Majunga.co.uk. All rights reserved.
+// </copyright>
+
+namespace BotDot.Helpers
+{
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// Helpers for file related taskss
+    /// </summary>
+    public class FileHelper
+    {
+        /// <summary>
+        /// Delete files in directory
+        /// </summary>
+        /// <param name="filePattern">Pattern to match with</param>
+        public void CleanUpFile(string filePattern)
+        {
+            foreach (var file in Directory.GetFiles(filePattern))
+            {
+                if (File.Exists(file))
+                {
+                    File.Delete(file);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Delete files in Directory that are older than 15 mins
+        /// </summary>
+        /// <param name="path">Directory Path</param>
+        public void DeleteExpiredFile(string path)
+        {
+            foreach (var file in Directory.GetFiles(path))
+            {
+                var fileInfo = new FileInfo(file);
+
+                if (fileInfo.CreationTime < DateTime.Now.AddMinutes(-15))
+                {
+                    fileInfo.Delete();
+                }
+            }
+        }
+    }
+}
