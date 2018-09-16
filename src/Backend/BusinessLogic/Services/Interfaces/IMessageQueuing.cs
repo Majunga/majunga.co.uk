1b15b0bea343ad3046afd6ee13f99879773965a2
Ethan McWilliams <ethan.mcwilliams@pharmacy2u.co.uk>
2018-09-16 20:32:21 +0100

    Added MQ Sending and Receiving Service and interface

---
+++ src/Backend/BusinessLogic/Services/Interfaces/IMessageQueuing.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="IMessageQueuing.cs" company="Majunga.co.uk">
+// Copyright (c) Majunga.co.uk. All rights reserved.
+// </copyright>
+
+namespace Backend.BusinessLogic.Services.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Backend.BusinessLogic.Services.Models.MessageQueue;
+
+    /// <summary>
+    /// Message Queue Interface
+    /// </summary>
+    public interface IMessageQueuing : IDi
+    {
+        /// <summary>
+        /// Queue Message
+        /// </summary>
+        /// <param name="serialisedMessage">Message to send</param>
+        void QueueMessage(string serialisedMessage);
+
+        /// <summary>
+        /// Queue Message
+        /// </summary>
+        /// <typeparam name="T">Generic Message Type</typeparam>
+        /// <param name="message">Message to send</param>
+        void QueueMessage<T>(T message);
+
+        /// <summary>
+        /// Read and pop message from Queue
+        /// </summary>
+        /// <returns>Serialised Message</returns>
+        string ReadMessageQueue();
+
+        /// <summary>
+        /// Count the number of messages in the Queue
+        /// </summary>
+        /// <returns>Count of messages</returns>
+        uint ReadQueueCount();
+
+        /// <summary>
+        /// Read and pop message from Queue
+        /// </summary>
+        /// <typeparam name="T">Generic Message Type</typeparam>
+        /// <returns>Message</returns>
+        T ReadMessageQueue<T>();
+    }
+}
