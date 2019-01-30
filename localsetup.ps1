0a9025f0be566759fcbf551aa47a8b66efae2811
Ethan McWilliams <ethan.mcwilliams@pharmacy2u.co.uk>
2019-01-30 13:47:34 +0000

    Updated to have a local setup for kubernetes
    Also have a remote version which relies on private repo credentials

--- localsetup.ps1
+++ localsetup.ps1
@@ -4,7 +4,12 @@ minikube start --vm-driver=hyperv --hyperv-virtual-switch "Default Switch" --mem
 Set-Location $currentLocation
 
 & minikube docker-env | Invoke-Expression
-docker build --rm -f ".\src\Backend\Dockerfile" -t backend:2 .\src\Backend
+docker build --rm -f ".\src\Backend\Dockerfile" -t backend:1 .\src\Backend
 docker build --rm -f ".\src\Frontend\Dockerfile" -t frontend:1 .\src\Frontend
 
-kubectl create -f .\deployment.yml
+kubectl create -f .\deployment.local.yml
+
+. kubernetes.ps1
+
+get-minikube-ip frontend
+get-minikube-ip backend
